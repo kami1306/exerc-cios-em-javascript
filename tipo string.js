58518e5847d213e7dddfae283aff53f3fd678bf6
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-03-22 08:17:36 -0300

    Add files via upload

---
+++ tipo string.js
@@ -0,0 +1,19 @@
+//tipo string
+
+const texto1="ola,mundo!";
+const texto2="ola,mundo!";
+const senha="senha@supersegura456!";
+const stringdenumeros="4567";
+
+const citacao="meu nome e";
+const meunome="kamille";
+
+          //concatenacao(+)
+
+          console.log(texto1)
+          console.log(texto2)
+          console.log(senha)
+          console.log(stringdenumeros)
+          console.log(citacao+meunome)
+
+          //template string OU template literal.
