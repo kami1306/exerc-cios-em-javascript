7a99d3eaab703c19682476b9ce4ae355b382195e
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-03-22 07:37:43 -0300

    Add files via upload

---
+++ tipo number.js
@@ -0,0 +1,17 @@
+//tipo number
+
+const meunumero=3;
+
+const primeironumero=1
+const segundonumero =2;
+const terceironumero=2.1;
+
+let soma=primeironumero+segundonumero;
+let subtracao=primeironumero-segundonumero
+let multiplicacao=primeironumero*segundonumero
+let divisao=primeironumero/segundonumero
+
+console .log("a soma e",soma)
+console .log("a subtracao e",subtracao)
+console .log("a multiplicacao e",multiplicacao)
+console .log("a divisao e","\n")
