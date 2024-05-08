b51f7846c49e1d4b5948a83b8e6ffda45252d4b0
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-05-08 11:25:01 -0300

    Add files via upload

--- desafio1.js
+++ desafio1.js
@@ -1,20 +1,24 @@
-// const nota1=10;
-// const nota2=6.5;
-// const nota3=8;
-// const nota4=7.5;
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
 
-// const media=(nota1+nota2+nota3+nota4)/4;
+//Ctrl + K, ctrl + C Comentar um bloco do código.
+// const media = (nota1 + nota2 + nota3 + nota4) / 4;
 // console.log(media)
 
+const notas = [10, 6.5, 8., 7.5];
 
-
-const notas=[10,6.5,8.,7.5];
-
-const media=(notas[0]+notas[2]+notas[3])/4;
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media)
+console.log(notas.length)
 
-console.log(notas[3])
+// Para nos permitir esse dinamismo, vamos conhecer a propriedade length que, em ingles, significa
+
+const exemplo =["Kamille",10,true]
+
+
 
 
 
