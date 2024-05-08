2870f306c51547340c25bdeea319bf0dba37a1da
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-05-08 12:06:08 -0300

    Add files via upload

---
+++ desafio4.js
@@ -0,0 +1,34 @@
+const alunos = [
+    "joão",
+    "juliana",
+    "ana",
+    "caio",
+    "lara",
+    "marjorie",
+    "guilherme",
+    "aline",
+    "fabiana",
+    "andre",
+    "carlos",
+    "paulo",
+    "bia",
+    "vivian",
+    "isabela",
+    "vinícius",
+    "renan",
+    "renata",
+    "daisy",
+    "camilo",
+];
+
+
+
+
+
+
+
+const sala1 = alunos.slice(0, alunos.length / 2);
+const sala2 = alunos.slice(alunos.length / 2);
+
+console.log(sala1);
+console.log(sala2);
