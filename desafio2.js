525ae07baba3cb293714df0094afc9476b84e7e8
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-05-08 11:31:47 -0300

    Add files via upload

--- desafio2.js
+++ desafio2.js
@@ -1,9 +1,9 @@
-const notas=[10,6,8];
+const notas = [10, 6, 8];
 
 notas.push(7);
 
-const media=(notas[0]+notas[1]+notas[2])/4;
-
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+ 
 console.log(media)
 
 console.log(notas[0],notas[1],notas[2],notas[3])
