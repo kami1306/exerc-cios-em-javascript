926201d66fb334a71bb1c9b76394bca249e90b07
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-05-09 08:36:23 -0300

    Add files via upload

---
+++ desafio8.js
@@ -0,0 +1,24 @@
+const alunos = ["joão", "juliana", "ana", "caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEmedias = [alunos,medias];
+
+function exibeNomeeNota(aluno){
+    if (listaDeAlunosEmedias[0].includes(aluno)){ 
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
